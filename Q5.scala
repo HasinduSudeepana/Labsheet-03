cc90a6a83f20faca18b7d83eab0a21b8ee95a4ab
Hasindu Sudeepana <108721333+HasinduSudeepana@users.noreply.github.com>
2022-07-18 11:05:28 +0530

    Update Q5.scala
    I added answer to Q 5

--- Q5.scala
+++ Q5.scala
@@ -2,20 +2,20 @@ object Q5
 {
     def main(args:Array[String]):Unit=
     {
-        println("Total minites:"+total_time(8,7))
+        println("Total minites:"+total_time(8,7))   // I pass the time(min) per km
     }
 
-    def easy_pace(x:Int):Double=
+    def easy_pace(x:Int):Double=  //this is the calculate time for go to the 2km as easy pace
+    { 
+        return 2.08 * x
+    }
+
+    def tempo_type(y:Int):Double=  //this is the calculate time for go to the 3km as tempo time
     {
-        return 2.0/x
+        return 3.0 * y
     }
 
-    def tempo_type(y:Int):Double=
-    {
-        return 3.0/y
-    }
-
-    def total_time(x:Int,y:Int):Double=
+    def total_time(x:Int,y:Int):Double=    //this is the formular calculate the total time
     {
         return easy_pace(x)+tempo_type(y)+easy_pace(x)
     }
