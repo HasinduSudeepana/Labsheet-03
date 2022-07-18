1f1d098874efa016fd5ae8c2d455410af0a7f052
Hasindu Sudeepana <108721333+HasinduSudeepana@users.noreply.github.com>
2022-07-18 11:19:15 +0530

    I added an answer to the Q3

--- Q3.scala
+++ Q3.scala
@@ -3,7 +3,7 @@ object Q3
     def main (args:Array[String]):Unit=
     {
         val r:Int=5
-        val v:Double=((4.0/3.0)*math.Pi*r*r*r)
+        val v:Double=((4.0/3.0)*math.Pi*r*r*r) //i use the pi value for math.pi
 
         println("Volume os a sphere with radius 5= "+v)
     }
